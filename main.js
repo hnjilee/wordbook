bc86309468090506c821f2027c97b8da2e342cb6
hnjilee <hnjilee@gmail.com>
2023-05-05 11:23:45 +0900

    Set check and btn click listeners

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 'use strict';
 
-loadWords('words.json').then(displayWords);
+loadWords('words.json')
+  .then(displayWords)
+  .then(() => {
+    const checked = [];
+    setCheckListener(checked);
+    setBtnClickListener(checked);
+  });
 
 async function loadWords(url) {
   const response = await fetch(url);
@@ -26,4 +32,45 @@ function createWordElement({ term, definition }) {
     `;
 
   return wordElement;
+}
+
+function setCheckListener(checked) {
+  const words = document.querySelector('.words');
+  words.addEventListener('change', e => {
+    const target = e.target;
+    if (target.checked) {
+      checked.push(target);
+    } else {
+      const index = checked.indexOf(target);
+      checked.splice(index, 1);
+    }
+  });
+}
+
+function setBtnClickListener(checked) {
+  const startBtn = document.querySelector('.header__start');
+  const scrollBtn = document.querySelector('.header__scroll');
+
+  startBtn.addEventListener('click', () => {
+    if (
+      !confirm('시작하면 체크된 목록을 수정할 수 없습니다. 시작하겠습니까?')
+    ) {
+      return;
+    }
+
+    const checkboxes = document.querySelectorAll('.word__checkbox');
+    checkboxes.forEach(box => box.setAttribute('disabled', 'true'));
+
+    startBtn.classList.add('header__btn--hidden');
+    scrollBtn.classList.remove('header__btn--hidden');
+  });
+
+  let i = 0;
+  scrollBtn.addEventListener('click', () => {
+    if (checked.length === 0) {
+      return;
+    }
+    checked[i++].scrollIntoView({ block: 'center', behavior: 'smooth' });
+    i = i >= checked.length ? 0 : i;
+  });
 }
